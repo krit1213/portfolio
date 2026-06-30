5cedc11ed5cb84bb3a86ee587f864cc3f3af086b
Kaleeswaran Krithiga <krithigak@hotmail.com>
2026-06-30 13:24:59 +0800

    Update portfolio content

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ const PORTFOLIO = {
   "name": "Krithiga Kaleeswaran",
   "tagline": "Learning by building!",
   "intro": "Hi, I am Krithiga. I enjoy building products that solve real problems, especially at the intersection of AI and technology. I am always curious to learn, experiment with new ideas, and turn them into something people genuinely find useful.",
-  "photo": "",
+  "photo": "assets/Kaleeswaran_Krithiga.jpg",
   "email": "krithigak@hotmail.com",
   "github": "https://github.com/krit1213",
   "linkedin": "linkedin.com/in/krithigakaleeswaran",
